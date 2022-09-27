c9c25b54be0c1ee5e494747351f21efb414d05f5
nmank <nathan.mankovich@gmail.com>
2022-09-27 10:00:46 -0600

    removed unnecessary files

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PathwayAnalysis",
-    version="0.0.3",
+    name="FlagIRLS",
+    version="0.0.1",
     author="Nate Mankovich",
     author_email="Nate.Mankovich@colostate.edu",
-    description="A package for biological pathway analysis.",
+    description="A package for subspace averaging and clustering",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nmank/PathwayAnalysis",
+    url="https://github.com/nmank/FlagIRLS",
     project_urls={
-        "Bug Tracker": "https://github.com/nmank/PathwayAnalysis/issues",
+        "Bug Tracker": "https://github.com/nmank/FlagIRLS/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
